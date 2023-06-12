2ab302d98e2eef85536b7fe19ae1e68a80eb3d29
Erwin-Veenhoven <erwinveenhoven2907@gmail.com>
2023-06-12 10:37:16 +0200

    update

---
+++ public/js/dataPage.js
@@ -0,0 +1,53 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    let temperatureData = google.visualization.arrayToDataTable([
+        ['Time', 'Temperature'],
+        ['01:01',  25],
+        ['02:02',  22],
+        ['03:03',  28],
+        ['04:04',  31],
+        ['05:05',  27]
+    ]);
+
+    let temperatureOptions = {
+        title: 'Temperature',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        colors: ['#ff6e6e'],
+        hAxis: { title: 'Time' },
+        vAxis: {
+            title: 'Temperature (°C)',
+            baselineColor: 'transparent',
+            // viewWindow: {
+            //     min: 0,
+            //     max: 40
+            // }
+        }
+    };
+
+    let rainfallData = google.visualization.arrayToDataTable([
+        ['Time', 'Rainfall'],
+        ['01-06-23',  5],
+        ['02-06-23',  0],
+        ['03-06-23',  2],
+        ['04-06-23',  3],
+        ['05-06-23',  0]
+    ]);
+    let rainfallOptions = {
+        title: 'Rainfall',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        colors: ['#49acff'],
+        hAxis: { title: 'Time' },
+        vAxis: {
+            title: 'Rainfall (mm)',
+            baselineColor: 'transparent'}
+    };
+
+    let tChart = new google.visualization.LineChart(document.getElementById('temperatureChart'));
+    tChart.draw(temperatureData, temperatureOptions);
+
+    let rChart = new google.visualization.LineChart(document.getElementById('rainfallChart'));
+    rChart.draw(rainfallData, rainfallOptions);
+}
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,4 +2,18 @@
 </style>
 
 <header>
+    <nav>
+        <ul class="navbar" >
+            <li><a href="/home">Home</a></li>
+            <li><a href="/data">Stations</a></li>
+            <li><a href="/data">Historical Data</a></li>
+
+            {{ Auth::user() }}
+            @if(!Auth::user() == null)
+                <li style="float: right;"><a href="{{ route("logout") }}">Logout</a></li>
+            @else
+                <li style="float: right;"><a href="{{ route("login") }}">Login</a></li>
+            @endif
+        </ul>
+    </nav>
 </header>
