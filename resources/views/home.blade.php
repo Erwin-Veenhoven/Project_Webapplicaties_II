09aa05c80dde7cf45a64976bfa58580d7f80c893
Erwin-Veenhoven <erwinveenhoven2907@gmail.com>
2023-06-20 16:14:08 +0200

    eem kiekn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,15 @@
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 
     <section>
-        <div class="info">
-            <div class="container left">
+        <div class="container">
+            <div class="card">
                 <h1>About</h1>
                 <p>CEVA Logistics has a global workforce of more than 110,000 people across 1,300 sites and more than 170 countries.</p>
             </div>
-
-            <div class="container center">
+            <div class="card">
                 <img src="{{ asset('img/background.jpg') }}" alt="Ceva" id="ceva">
             </div>
-
-            <div class="container right">
+            <div class="card">
                 <h1>Website</h1>
                 <p>This is a page where employees can check the weather data. If you are a customer or would like to become one, the button below will lead you to our main page for customers.</p>
                 <a href="https://www.cevalogistics.com/" id="ceva-button" class="button">Ceva</a>
